8ba85f2fb7a19f85de03b61db9d746abccde6940
Fluctlight Kayaba <kayaba.fluctlight@gmail.com>
2023-10-07 14:34:17 +0700

    pseudo token minting with metaplex

--- programs/engine/src/lib.rs
+++ programs/engine/src/lib.rs
@@ -1,8 +1,5 @@
 use anchor_lang::prelude::*;
-use anchor_spl::{
-	token,
-	token::{MintTo, Token},
-};
+use anchor_spl::{associated_token::AssociatedToken, token::Token};
 
 declare_id!("2sH2Q2kcFvWpxjXqv78THttyfVKS25eNFvuD3vaj6BiN");
 
@@ -10,31 +7,57 @@ declare_id!("2sH2Q2kcFvWpxjXqv78THttyfVKS25eNFvuD3vaj6BiN");
 pub mod carbon_engine {
 	use super::*;
 
-	pub fn mint_token(ctx: Context<MintToken>) -> Result<()> {
-		let cpi_accounts = MintTo {
-			mint: ctx.accounts.mint.to_account_info(),
-			to: ctx.accounts.token_account.to_account_info(),
-			authority: ctx.accounts.payer.to_account_info(),
-		};
+	pub fn mint(ctx: Context<MintNft>) -> Result<()> {
+		msg!("Creating mint account...");
+		msg!("Mint: {}", &ctx.accounts.mint.key());
 
-		let cpi_program = ctx.accounts.token_program.to_account_info();
-		let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+		msg!("Initializing mint account...");
+		msg!("Mint: {}", &ctx.accounts.mint.key());
 
-		token::mint_to(cpi_ctx, 10)?;
+		msg!("Creating token account...");
+		msg!("Token Address: {}", &ctx.accounts.token_account.key());
+
+		msg!("Minting token to account...");
+		msg!("Mint: {}", &ctx.accounts.mint.to_account_info().key());
+		msg!("Token Address: {}", &ctx.accounts.token_account.key());
+
+		msg!("Creating metadata acount...");
+		msg!(
+			"Metadata acount address: {}",
+			&ctx.accounts.metadata.to_account_info().key(),
+		);
+
+		msg!("Create master edition metadata account...");
+		msg!(
+			"Master edition metadata acount address: {}",
+			&ctx.accounts.master_edition.to_account_info().key(),
+		);
+
+		msg!("Token mint process completed successfully!");
+
 		Ok(())
 	}
 }
 
 #[derive(Accounts)]
-pub struct MintToken<'info> {
-	/// CHECK: This not dangerous because we don't read/write
+pub struct MintNft<'info> {
+	/// CHECK: This part handled by Metaplex
 	#[account(mut)]
-	pub mint: UncheckedAccount<'info>,
-	pub token_program: Program<'info, Token>,
-	/// CHECK: This not dangerous because we don't read/write
+	pub metadata: UncheckedAccount<'info>,
+	#[account(mut)]
+	/// CHECK: We're about to create this with Metaplex
+	pub master_edition: UncheckedAccount<'info>,
+	#[account(mut)]
+	pub mint: Signer<'info>,
+	/// CHECK: We're about to create this with Anchor
 	#[account(mut)]
 	pub token_account: UncheckedAccount<'info>,
-	/// CHECK: This not dangerous because we don't read/write
 	#[account(mut)]
-	pub payer: AccountInfo<'info>,
+	pub mint_authority: Signer<'info>,
+	pub rent: Sysvar<'info, Rent>,
+	pub system_program: Program<'info, System>,
+	pub token_program: Program<'info, Token>,
+	pub associated_token_program: Program<'info, AssociatedToken>,
+	/// CHECK: Metaplex will check this
+	pub token_metadata_program: UncheckedAccount<'info>,
 }
