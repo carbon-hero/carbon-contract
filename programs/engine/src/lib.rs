8253adfddf170fd78293da36a5b286b6cca9ff40
Fluctlight Kayaba <kayaba.fluctlight@gmail.com>
2023-10-07 15:09:09 +0700

    instruction to mint Metaplex NFT

--- programs/engine/src/lib.rs
+++ programs/engine/src/lib.rs
@@ -1,5 +1,6 @@
-use anchor_lang::prelude::*;
-use anchor_spl::{associated_token::AssociatedToken, token::Token};
+use anchor_lang::{prelude::*, solana_program::program::invoke, system_program};
+use anchor_spl::{associated_token, associated_token::AssociatedToken, token, token::Token};
+use mpl_token_metadata::{instructions as token_instructions, ID as TOKEN_METADATA_ID};
 
 declare_id!("2sH2Q2kcFvWpxjXqv78THttyfVKS25eNFvuD3vaj6BiN");
 
@@ -10,16 +11,62 @@ pub mod carbon_engine {
 	pub fn mint(ctx: Context<MintNft>) -> Result<()> {
 		msg!("Creating mint account...");
 		msg!("Mint: {}", &ctx.accounts.mint.key());
+		system_program::create_account(
+			CpiContext::new(
+				ctx.accounts.token_program.to_account_info(),
+				system_program::CreateAccount {
+					from: ctx.accounts.mint_authority.to_account_info(),
+					to: ctx.accounts.mint.to_account_info(),
+				},
+			),
+			10_000_000,
+			82,
+			&ctx.accounts.token_program.key(),
+		)?;
 
 		msg!("Initializing mint account...");
 		msg!("Mint: {}", &ctx.accounts.mint.key());
+		token::initialize_mint(
+			CpiContext::new(
+				ctx.accounts.token_program.to_account_info(),
+				token::InitializeMint {
+					mint: ctx.accounts.token_program.to_account_info(),
+					rent: ctx.accounts.rent.to_account_info(),
+				},
+			),
+			0,
+			&ctx.accounts.mint_authority.key(),
+			Some(&ctx.accounts.mint_authority.key()),
+		)?;
 
 		msg!("Creating token account...");
 		msg!("Token Address: {}", &ctx.accounts.token_account.key());
+		associated_token::create(CpiContext::new(
+			ctx.accounts.associated_token_program.to_account_info(),
+			associated_token::Create {
+				payer: ctx.accounts.mint_authority.to_account_info(),
+				associated_token: ctx.accounts.token_account.to_account_info(),
+				authority: ctx.accounts.mint_authority.to_account_info(),
+				mint: ctx.accounts.mint.to_account_info(),
+				system_program: ctx.accounts.associated_token_program.to_account_info(),
+				token_program: ctx.accounts.token_program.to_account_info(),
+			},
+		))?;
 
 		msg!("Minting token to account...");
 		msg!("Mint: {}", &ctx.accounts.mint.to_account_info().key());
 		msg!("Token Address: {}", &ctx.accounts.token_account.key());
+		token::mint_to(
+			CpiContext::new(
+				ctx.accounts.token_program.to_account_info(),
+				token::MintTo {
+					mint: ctx.accounts.mint.to_account_info(),
+					to: ctx.accounts.token_account.to_account_info(),
+					authority: ctx.accounts.associated_token_program.to_account_info(),
+				},
+			),
+			1,
+		)?;
 
 		msg!("Creating metadata acount...");
 		msg!(
